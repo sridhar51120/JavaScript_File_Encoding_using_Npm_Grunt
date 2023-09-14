64f37b8c260b30c8cc13e7538088162723e395ef
SRIDHAR DSCV <sridhardscv@gmail.com>
2023-09-14 23:59:07 +0530

    initial template

--- project/grunt/Gruntfile.js
+++ project/grunt/Gruntfile.js
@@ -1,5 +1,26 @@
 module.exports = function (grunt) {
     grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                separator: '\n/*next file*/\n\n'  
+            },
+            dist: {
+                src: ['../scripts/hello.js', '../scripts/main.js'],
+                dest: 'scripts/built.js'
+            }
+        },
 
+        uglify: {
+            build: {
+                files: {
+                    '../../htdocs/js/main.min.js': ['../scripts/built.js']
+                }
+            }
+        }
     });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.registerTask('js', ['uglify','concat']);
 };
